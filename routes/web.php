16eae83ba274e53c3fb8e33f7231ce53fe80f46b
smunyaradzi <kmunyaradzi@gmail.com>
2022-05-16 12:40:22 +0400

    latest

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,13 @@ use PHPUnit\TextUI\XmlConfiguration\Group;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
